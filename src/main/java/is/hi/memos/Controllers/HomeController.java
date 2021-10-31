f3e27323d0105b6f351db64fa5f53d5c76deb8ef
Ramondlr <42787384+Ramondlr@users.noreply.github.com>
2021-10-31 01:18:39 +0000

    bætti við tímastimpil fyrir seinustu breytingu

--- src/main/java/is/hi/memos/Controllers/HomeController.java
+++ src/main/java/is/hi/memos/Controllers/HomeController.java
@@ -1,14 +1,30 @@
 package is.hi.memos.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 public class HomeController
 {
-    @RequestMapping
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String HomeController()
     {
-        return "";
+        return "home";
+    }
+
+    @RequestMapping(value = "/memo/{id}", method = RequestMethod.GET)
+    public String memoGET(Model model){
+
+        Date currentDate = new Date();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("YY:MM:dd | hh:mm:ss");
+        model.addAttribute("lastSaved", timeFormat.format(currentDate));
+        return "memo";
     }
 }
